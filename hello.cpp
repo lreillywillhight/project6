15ff67cb4f49f999635db85a534f40b5be553f46
lreillywillhight <lreillywillhight@gmail.com>
2026-05-28 02:46:32 -0700

    hello

---
+++ hello.cpp
@@ -0,0 +1,29 @@
+/*
+Louis Reilly-Willhight 
+CS131 Spring 2026, Everett CC
+
+Employee Data II
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+
+int main() 
+{
+    
+    cout << "\n\nEND\n\n";
+
+    return 0;
+}
+
+/* ===== SAMPLE OUTPUT =========
+
+*/
+
+/* =====CODE GRAVEYARD=====
+
+
+*/
